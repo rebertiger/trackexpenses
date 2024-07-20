ee94f0b201d78d101a99d5691372756883c9afa4
Recep Berke KAPLAN <recepberkekaplan@gmail.com>
2024-07-20 10:48:44 +0300

    Add dark theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:trackexpenses/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 212, 123, 227),brightness: Brightness.dark,);
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme, 
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 13),
+      ),
+    ),
+    theme: ThemeData().copyWith(
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 13),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer)),
+          textTheme: ThemeData().textTheme.copyWith(titleLarge: TextStyle(fontWeight: FontWeight.bold,color: kColorScheme.onSecondaryContainer,fontSize: 16,)),
+    ),
+    themeMode: ThemeMode.system,
+    home: const Expenses(),
   ));
 }
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -9,12 +9,13 @@ class ExpenseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.blue,
+      color: const Color.fromARGB(255, 212, 123, 227),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(expense.title,style: Theme.of(context).textTheme.titleLarge),
             const SizedBox(
               height: 4,
             ),
